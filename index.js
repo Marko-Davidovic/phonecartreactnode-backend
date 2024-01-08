c329155df90cb8426c42708392d4f9a03fd870c3
Marko Davidovic <davidovic-m@hotmail.com>
2024-01-08 13:33:26 -0500

    redirect test 2

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ const app = express();
 
 require("dotenv").config();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://phone-cart-fullstack.netlify.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.json());
 app.use(cors());
 
